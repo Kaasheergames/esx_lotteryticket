9985ebe7b4c2dc7c79cf7b089778fe3c82a0de3d
Kaasheergames <121974307+Kaasheergames@users.noreply.github.com>
2023-01-26 18:05:27 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game "gta5"
+
+name "esx_lotteryticket"
+description "Lottery Ticket"
+author "Kaasheergames - https://github.com/Kaasheergames"
+
+server_scripts {
+	'server/server.lua'
+}
